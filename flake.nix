9803dedc490abb9748f85e766f1331eea1c4a4bf
Ethan Moss <74180458+ethanmoss1@users.noreply.github.com>
2025-08-04 15:00:17 +0100

    flake.nix: module options
    T2 Macs are based on x86_64, therefore we can remove the flakes part dependency.
    nixosModules set up, you can run the service and also set up individual fans.

--- flake.nix
+++ flake.nix
@@ -1,29 +1,105 @@
 {
-  description = "T2 fan daemon ported to rust";
+  description = "NixOS module and package for t2fanrd";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem =
-        {
-          pkgs,
-          ...
-        }:
-        {
-          formatter = pkgs.nixfmt-rfc-style;
-          packages.default = pkgs.rustPlatform.buildRustPackage {
-            pname = "t2fanrd";
-            version = "0.1.0";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux"; # As this is for T2 Macs, only x86_64 needed.
+      pkgs = nixpkgs.legacyPackages.${system};
 
-            src = ./.;
-            cargoHash = "sha256-RD443SBsDbrJ0Yq3yO23RIZa2Wyi0EnyuX61VmwHZIk=";
+      # The Package Definition
+      t2fanrdPackage = pkgs.rustPlatform.buildRustPackage {
+        pname = "t2fanrd";
+        version = "0.1.0";
+        src = ./.;
+        cargoHash = "sha256-FKQYiaOTZxD95AWD2zbVjENzMAPrFl/rzhwbkAgGbx0=";
+      };
+
+      # The NixOS module definition.
+      t2fanrdModule = { config, lib, ... }:
+        let
+          cfg = config.services.t2fanrd;
+
+          # A helper function to generate the fan configuration string.
+          fanConfig = fan: ''
+            [${fan.name}]
+            low_temp=${toString fan.lowTemp}
+            high_temp=${toString fan.highTemp}
+            speed_curve=${fan.speedCurve}
+            always_full_speed=${if fan.alwaysFullSpeed then "true" else "false"}
+          '';
+        in
+          {
+            options.services.t2fanrd = {
+              enable = lib.mkEnableOption "t2fanrd daemon to manage fan curves for T2 Macs";
+
+              fans = lib.mkOption {
+                type = lib.types.listOf
+                  (lib.types.submodule ({ lib, ... }: {
+                    options = {
+                      name = lib.mkOption {
+                        type = lib.types.str;
+                        description = "The name of the fan section in the configuration file (e.g., 'Fan1').";
+                      };
+
+                      lowTemp = lib.mkOption {
+                        type  = lib.types.int;
+                        default = 55;
+                        example = 40;
+                        description = "Temperature in Celsius that will trigger a higher fan speed.";
+                      };
+
+                      highTemp = lib.mkOption {
+                        type  = lib.types.int;
+                        default = 75;
+                        example = 80;
+                        description = "Temperature in Celsius that will trigger a higher fan speed.";
+                      };
+
+                      speedCurve = lib.mkOption {
+                        type = lib.types.enum [ "linear" "exponential" "logarithmic" ];
+                        default = "linear";
+                        example = "logarithmic";
+                        description = "Sets the fan speed curve.";
+                      };
+
+                      alwaysFullSpeed = lib.mkOption {
+                        type = lib.types.bool;
+                        default = false;
+                        example = true;
+                        description = "If true, the fan will be at max speed regardless of temperature.";
+                      };
+                    };
+                  }));
+              };
+            };
+
+            config = lib.mkIf cfg.enable {
+              systemd.services.t2fanrd = {
+                description = "T2FanRD daemon to manage the fans on a T2 Mac";
+                wantedBy = [ "multi-user.target" ];
+                serviceConfig = {
+                  Type = "exec";
+                  ExecStart = "${t2fanrdPackage}/bin/t2fanrd";
+                  Restart = "always";
+                };
+                # https://nixos.org/manual/nixos/stable/options#opt-systemd.services._name_.reloadTriggers
+                restartTriggers = [ config.environment.etc."t2fand.conf".source ];
+              };
+
+              environment.systemPackages = [ t2fanrdPackage ];
+              environment.etc."t2fand.conf".text = builtins.concatStringsSep "\n" (map fanConfig cfg.fans);
+            };
           };
-        };
-    };
+    in
+      {
+        # This makes the package available to your system configuration.
+        packages.${system}.default = t2fanrdPackage;
+
+        # This makes the NixOS module available for import.
+        nixosModules.t2fanrd = t2fanrdModule;
+      };
 }
