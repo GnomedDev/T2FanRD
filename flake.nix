dcd1985516cbc78c1e39ee6c38600eefdde81400
Ethan Moss <cywinskimoss@gmail.com>
2025-09-15 18:17:10 +0100

    FIX: indentation and merged

--- flake.nix
+++ flake.nix
@@ -16,54 +16,57 @@
         src = ./.;
         cargoHash = "sha256-FKQYiaOTZxD95AWD2zbVjENzMAPrFl/rzhwbkAgGbx0=";
       };
-      nixosModules.t2fanrd = { config, lib, ... }: {
-        options.services.t2fanrd = {
-          enable = lib.mkEnableOption "t2fanrd daemon to manage fan curves for T2 Macs";
+      nixosModules.t2fanrd = { config, lib, ... }:
+        let
+          cfg = config.services.t2fanrd;
+        in {
+          options.services.t2fanrd = {
+            enable = lib.mkEnableOption "t2fanrd daemon to manage fan curves for T2 Macs";
 
-          package = lib.mkOption {
-            type = lib.types.package;
-            default = self.packages.${system}.default;
-            description = "T2fanrd package to use.";
-          };
-          config = lib.mkOption {
-            type = lib.types.attrsOf (lib.types.submodule {
-              options = {
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${system}.default;
+              description = "T2fanrd package to use.";
+            };
+            config = lib.mkOption {
+              type = lib.types.attrsOf (lib.types.submodule {
+                options = {
 
-                low_temp = lib.mkOption {
-                  type  = lib.types.int;
-                  default = 55;
-                  example = 40;
-                  description = "Temperature in Celsius that will trigger a higher fan speed.";
+                  low_temp = lib.mkOption {
+                    type  = lib.types.int;
+                    default = 55;
+                    example = 40;
+                    description = "Temperature in Celsius that will trigger a higher fan speed.";
+                  };
+
+                  high_temp = lib.mkOption {
+                    type  = lib.types.int;
+                    default = 75;
+                    example = 80;
+                    description = "Temperature in Celsius that will trigger a higher fan speed.";
+                  };
+
+                  speed_curve = lib.mkOption {
+                    type = lib.types.enum [ "linear" "exponential" "logarithmic" ];
+                    default = "linear";
+                    example = "logarithmic";
+                    description = "Sets the fan speed curve.";
+                  };
+
+                  always_full_speed = lib.mkOption {
+                    type = lib.types.bool;
+                    default = false;
+                    example = true;
+                    description = "If true, the fan will be at max speed regardless of temperature.";
+                  };
                 };
-
-                high_temp = lib.mkOption {
-                  type  = lib.types.int;
-                  default = 75;
-                  example = 80;
-                  description = "Temperature in Celsius that will trigger a higher fan speed.";
-                };
-
-                speed_curve = lib.mkOption {
-                  type = lib.types.enum [ "linear" "exponential" "logarithmic" ];
-                  default = "linear";
-                  example = "logarithmic";
-                  description = "Sets the fan speed curve.";
-                };
-
-                always_full_speed = lib.mkOption {
-                  type = lib.types.bool;
-                  default = false;
-                  example = true;
-                  description = "If true, the fan will be at max speed regardless of temperature.";
-                };
-              };
-            });
-            default = {};
-            description = ''
+              });
+              default = {};
+              description = ''
              An attribute set where each attribute is a fan to configure
              with its settings.
             '';
-            example = lib.literalExample ''{
+              example = lib.literalExample ''{
               Fan1 = {
                 low_temp = 40;
                 high_temp = 70;
@@ -76,21 +79,21 @@
                 speed_curve = "linear";
                 always_full_speed = false;
               };
-            };'';
-          };
-        };
-      };
-          config = lib.mkIf cfg.enable {
-          systemd.services.t2fanrd = {
-            description = "T2FanRD daemon to manage the fans on a T2 Mac";
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig = {
-              Type = "exec";
-              ExecStart = "${cfg.package}/bin/t2fanrd";
-              Restart = "always";
+              };'';
             };
           };
-          environment.etc."t2fand.conf".source = ((pkgs.formats.toml { }).generate "t2fand.conf" config.services.t2fanrd.config);
+          config = lib.mkIf cfg.enable {
+            systemd.services.t2fanrd = {
+              description = "T2FanRD daemon to manage the fans on a T2 Mac";
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                Type = "exec";
+                ExecStart = "${cfg.package}/bin/t2fanrd";
+                Restart = "always";
+              };
+            };
+            environment.etc."t2fand.conf".source = ((pkgs.formats.toml { }).generate "t2fand.conf" cfg.config);
+          };
         };
     };
 }
