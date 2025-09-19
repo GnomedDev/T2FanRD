02236a86a1be7f14db2fbc16fe3861b762255735
Ethan Moss <74180458+ethanmoss1@users.noreply.github.com>
2025-09-19 18:38:42 +0100

    Upstream Nix flake and module configuration. (#4)
    Co-authored-by: Cassie Cheung <me@soopy.moe>

---
+++ flake.nix
@@ -0,0 +1,124 @@
+{
+  description = "NixOS module and package for t2fanrd";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux"; # As this is for T2 Macs, only x86_64 needed.
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "t2fanrd";
+        version = "0.1.0";
+        src = ./.;
+        cargoHash = "sha256-FKQYiaOTZxD95AWD2zbVjENzMAPrFl/rzhwbkAgGbx0=";
+      };
+      nixosModules.t2fanrd = { config, lib, ... }:
+        let
+          cfg = config.services.t2fanrd;
+        in {
+          options.services.t2fanrd = {
+            enable = lib.mkEnableOption "t2fanrd daemon to manage fan curves for T2 Macs";
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${system}.default;
+              description = "T2fanrd package to use.";
+            };
+            config = lib.mkOption {
+              type = lib.types.attrsOf (lib.types.submodule {
+                options = {
+
+                  low_temp = lib.mkOption {
+                    type  = lib.types.int;
+                    default = 55;
+                    example = 40;
+                    description = "Temperature in Celsius that will trigger a higher fan speed.";
+                  };
+
+                  high_temp = lib.mkOption {
+                    type  = lib.types.int;
+                    default = 75;
+                    example = 80;
+                    description = "Temperature in Celsius that will trigger a higher fan speed.";
+                  };
+
+                  speed_curve = lib.mkOption {
+                    type = lib.types.enum [ "linear" "exponential" "logarithmic" ];
+                    default = "linear";
+                    example = "logarithmic";
+                    description = "Sets the fan speed curve.";
+                  };
+
+                  always_full_speed = lib.mkOption {
+                    type = lib.types.bool;
+                    default = false;
+                    example = true;
+                    description = "If true, the fan will be at max speed regardless of temperature.";
+                  };
+                };
+              });
+              default = {};
+              description = ''
+                An attribute set where each attribute is a fan to configure
+                with its settings.
+              '';
+              example = lib.literalExample ''{
+                Fan1 = {
+                  low_temp = 40;
+                  high_temp = 70;
+                  speed_curve = "linear";
+                  always_full_speed = false;
+                };
+                Fan2 = {
+                  low_temp = 40;
+                  high_temp = 70;
+                  speed_curve = "linear";
+                  always_full_speed = false;
+                };
+              };'';
+            };
+          };
+          config = lib.mkIf cfg.enable {
+            systemd.services.t2fanrd = {
+              description = "T2FanRD daemon to manage the fans on a T2 Mac";
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                Type = "exec";
+                ExecStart = "${cfg.package}/bin/t2fanrd";
+                Restart = "always";
+
+                # Taken from the fedora system service
+                # https://github.com/t2linux/fedora/blob/2947fdc909a35f04eb936a4f9c0f33fe4e52d9c2/t2fanrd/t2fanrd.service
+                PrivateTmp = "true";
+                ProtectSystem = "true";
+                ProtectHome = "true";
+                ProtectClock = "true";
+                ProtectControlGroups = "true";
+                ProtectHostname = "true";
+                ProtectKernelLogs = "true";
+                ProtectKernelModules = "true";
+                ProtectProc = "invisible";
+                PrivateDevices = "true";
+                PrivateNetwork = "true";
+                NoNewPrivileges = "true";
+                DevicePolicy = "closed";
+                KeyringMode = "private";
+                LockPersonality = "true";
+                MemoryDenyWriteExecute = "true";
+                PrivateUsers = "yes";
+                RemoveIPC = "yes";
+                RestrictNamespaces = "yes";
+                RestrictRealtime = "yes";
+                RestrictSUIDSGID = "yes";
+                SystemCallArchitectures = "native";
+              };
+            };
+            environment.etc."t2fand.conf".source = ((pkgs.formats.toml { }).generate "t2fand.conf" cfg.config);
+          };
+        };
+    };
+}
